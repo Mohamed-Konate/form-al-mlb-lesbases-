4bb96272d47b3a7d604cd2b722ec7bc6c7d48565
Mohamed Konaté <ing.mohamedkonate@gmail.com>
2022-12-26 18:24:28 +0000

    Utiliser la propriété data

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const application = {
-
+    data(){
+        return {
+            title: "Hello World",
+            message: "Voici ma prémière application Vue js 3"
+        }
+    }
 }
 Vue.createApp(application).mount('#app')
