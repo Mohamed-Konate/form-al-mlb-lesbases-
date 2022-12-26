60a1ec143911b28b07498af2cdffc75d45534608
Mohamed Konaté <ing.mohamedkonate@gmail.com>
2022-12-26 18:38:34 +0000

    Utiliser des méthodes

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const application = {
     data(){
         return {
             title: "Hello World",
-            message: "Voici ma prémière application Vue js 3"
+            message: "Voici ma prémière application Vue js 3",
+            width: 500,
+            height: 300,
         }
+    },
+    methods: {
+        bonjour: function(){
+            return 'Bonjour tout le monde'
+        },
+        getSquareSurface: function (side){
+            return Math.pow(side, 2)
+        },
+        getRectangleSurface: function (){
+            return this.width * this.height
+        }
+
     }
 }
 Vue.createApp(application).mount('#app')
