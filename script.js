c18cf15fa9ba515a33a5e806be3cc21f3e5454a7
Mohamed Konaté <ing.mohamedkonate@gmail.com>
2022-12-30 08:36:25 +0000

    task

--- script.js
+++ script.js
@@ -30,6 +30,12 @@ const application = {
             if (!this.newUser.age || !Number.isInteger(this.newUser.age) || this.newUser.age <15){
                 this.errors.push('Age must be an integer greater than 15');
             }
+            if (this.errors.length){
+                return false;
+            }
+            this.users.push(this.newUser);
+            this.newUser = {};
+            return true;
         }
     },
 }
