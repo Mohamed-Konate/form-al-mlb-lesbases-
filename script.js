7744559560f1f81e0dca0388af1c11228206c3b6
Mohamed Konaté <ing.mohamedkonate@gmail.com>
2022-12-31 12:15:48 +0000

    todo list p1

--- script.js
+++ script.js
@@ -1,60 +1,15 @@
 const application = {
     data(){
         return {
-            title: "Hello World",
-            message: "Voici ma prémière application Vue js 3",
-            width: 500,
-            height: 300,
-            fruits: ['Orange', 'Banana', 'cherise', 'mango'],
-            lien: 'https://alphorm.com',
-            image: 'images/img.png',
-            animal: false,
-            message_v_model: 'Message par defaut',
-            done: false,
-            textHtml: '<p class="text-danger"> Attention !</p>',
-            km:0,
-            finish: 5
+            tasks: [
+                {name: 'Task 1', done: false},
+                {name: 'Task 2', done: true},
+            ]
         }
     },
     methods: {
-        bonjour: function(){
-            return 'Bonjour tout le monde'
-        },
-        getSquareSurface: function (side){
-            return Math.pow(side, 2)
-        },
-        getRectangleSurface: function (){
-            return this.width * this.height
-        },
-        redButton: function (){
-            return alert('Tu as cliqué sur le bouton rouge !')
-        },
-        blueButton: function (){
-            return alert('Tu as cliqué sur le bouton bleu!')
-        },
-        blackButton: function (){
-            return alert('La souris est sorti du bouton noir !')
-        },
-        drive: function(){
-            setInterval(()=>{
-                this.km++
-            }, 1000)
-        }
 
     },
-    beforeCreate(){
-        console.log(this.km)
-    },
-    created(){
-        this.drive()
-    },
-    watch: {
-        km: function(){
-            if (this.km===this.finish){
-                console.log('Tu es arrivé !')
-                this.km = 0;
-            }
-        }
-    }
+
 }
 Vue.createApp(application).mount('#app')
